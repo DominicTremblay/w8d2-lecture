cf36145cf81f162d367c67035ca5633a8ac2e2c0
Dominic Tremblay <dominictremblay24@gmail.com>
2023-11-21 00:25:43 -0500

    reset new version of nov 2023

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Superheros from './pages/Superheros';
 import Search from './pages/Search';
 import Login from './pages/Login';
-import useSuperheros from './hooks/useSuperheros';
 
 function App() {
-
-  const { state, dispatch } = useSuperheros();
-
   return (
     <div className="App">
       <Navbar />
       <Home />
       {/* <Superheros /> */}
       {/* <Search /> */}
-
       {/* <Login /> */}
     </div>
   );
